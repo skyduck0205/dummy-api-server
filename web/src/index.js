56375aa687e3f14b812f09e7516a6c7f30c57cd2
skyduck <skyduck0205@gmail.com>
2020-04-30 14:54:50 +0800

    chore(web): Add material-ui to libs

--- web/src/index.js
+++ web/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
