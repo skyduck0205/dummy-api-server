f9ae0cf7b807a427f424c97f16bf0da5838da4c7
skyduck <skyduck0205@gmail.com>
2020-05-04 17:11:21 +0800

    feat(server): Add API: get dummy API list

--- server/app.js
+++ server/app.js
@@ -5,25 +5,33 @@ const logger = require('morgan');
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 
-const DB_PATH = path.join(__dirname, process.env.DB || '../db/db.json');
+const DB = path.join(__dirname, process.env.DB || '../db/db.json');
 const PUBLIC_PATH = path.join(__dirname, '../public');
+const DS_PREFIX = process.env.DS_PREFIX || '/_ds';
 
 const app = express();
 
-// Initialize DB
-const db = low(new FileSync(DB_PATH));
-db.defaults({})
+/**
+ * Initialize DB
+ */
+const db = low(new FileSync(DB));
+db.defaults({ apis: [] })
   .write();
 
-// Server config
+/**
+ * Server config
+ */
 app.use(logger('dev'));
 app.use(cookieParser());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 app.use(express.static(PUBLIC_PATH));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+/**
+ * Dummy Server APIs
+ */
+// return full api list
+app.route(`${DS_PREFIX}/api`)
+  .get((req, res) => res.send(db.get('apis').value()));
 
 module.exports = app;
