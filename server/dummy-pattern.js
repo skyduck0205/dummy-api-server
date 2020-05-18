f5dd2c609f0bf6f7eb4b17b3119eeeab3e1f19bb
skyduck <skyduck0205@gmail.com>
2020-05-18 18:21:20 +0800

    feat(server/DummyPattern): Add matching methods and update tests

--- server/dummy-pattern.js
+++ server/dummy-pattern.js
@@ -28,6 +28,13 @@ class DummyPattern {
     }
   }
 
+  /**
+   * Transfer a pattern from path by serializing the url parameters and
+   * sorting the query params. The pathPattern in router should be unique.
+   * @example
+   * '/api/:param/:anotherParam?page&orderBy' will have a path pattern
+   * '/api/:0/:1?orderBy&page'
+   */
   get pathPattern() {
     let pathPattern = this.url;
     // replace url param names with number id
@@ -41,10 +48,23 @@ class DummyPattern {
     return pathPattern;
   }
 
+  /**
+   * Use url of API to create a UrlPattern instance
+   */
   parseUrlPattern() {
     this.urlPattern = new UrlPattern(this.url);
   }
 
+  /**
+   * Parse the query pattern of API path. If the query params are without
+   * value, their value in query pattern will be set to true.
+   * @example
+   * If a API path is '/user?search=John&page', its query pattern will be
+   * {
+   *   search: 'John',
+   *   page: true
+   * }
+   */
   parseQueryPattern() {
     const queryPattern = {};
     if (this.query) {
@@ -55,6 +75,45 @@ class DummyPattern {
     }
     this.queryPattern = queryPattern;
     debug('parseQueryPattern:', this.path, this.queryPattern);
+  }
+
+  /**
+   * Check path and query match with this pattern
+   * @param {string} path - url path of request
+   * @param {object} query - key-value query parameter of request
+   * @returns {boolean} - result
+   */
+  match(path, query) {
+    return (
+      this.matchUrl(path) &&
+      this.matchQuery(query)
+    );
+  }
+
+  /**
+   * Check if input url is matched with urlPattern
+   * @param {object} query - key-value query parameter of request
+   * @returns {boolean} result
+   */
+  matchUrl(path) {
+    return this.urlPattern.match(path);
+  }
+
+  /**
+   * Check if input query paramters are matched with queryPattern, determine if
+   * query parameters contains equivalent property values with queryPattern
+   * @param {string} path - url path of request
+   * @returns {boolean} result
+   */
+  matchQuery(query) {
+    return _.isMatchWith(
+      query,  // object - The object to inspect.
+      this.queryPattern,  // source - The object of property values to match.
+      // customizer - There are two conditions that treat source value and object value as equivalent
+      // 1. queryPattern property and query param property have the same value
+      // 2. queryPattern property value is true and param property value is not empty
+      (objVal, srcVal) => (objVal === srcVal || (srcVal === true && objVal))
+    );
   }
 }
 
--- server/test/dummy-pattern.test.js
+++ server/test/dummy-pattern.test.js
@@ -4,20 +4,20 @@ const DummyPattern = require('../dummy-pattern');
 const UrlPattern = require('url-pattern');
 
 describe('DummyPattern', () => {
-  let parseUrlStub;
-  let parseQueryStub;
-
-  beforeEach(() => {
-    parseUrlStub = sinon.stub(DummyPattern.prototype, 'parseUrlPattern');
-    parseQueryStub = sinon.stub(DummyPattern.prototype, 'parseQueryPattern');
-  });
-
-  afterEach(() => {
-    parseUrlStub.restore();
-    parseQueryStub.restore();
-  });
-
   describe('constructor', () => {
+    let parseUrlStub;
+    let parseQueryStub;
+
+    beforeEach(() => {
+      parseUrlStub = sinon.stub(DummyPattern.prototype, 'parseUrlPattern');
+      parseQueryStub = sinon.stub(DummyPattern.prototype, 'parseQueryPattern');
+    });
+
+    afterEach(() => {
+      parseUrlStub.restore();
+      parseQueryStub.restore();
+    });
+
     it('init attributes and call parseUrlPattern, parseQueryPattern', () => {
       const dp = new DummyPattern('/user');
       assert.equal(dp.path, '/user');
@@ -40,12 +40,14 @@ describe('DummyPattern', () => {
     });
 
     it('return empty string if path is empty', () => {
-      const dp = new DummyPattern('');
+      const dp = new DummyPattern('/url');
+      dp.path = '';
       assert.equal(dp.url, '');
     });
 
     it('return empty string if path is not a string', () => {
-      const dp = new DummyPattern(null);
+      const dp = new DummyPattern('/url');
+      dp.path = 123;
       assert.equal(dp.url, '');
     });
   });
@@ -62,7 +64,8 @@ describe('DummyPattern', () => {
     });
 
     it('return empty string if path is not a string', () => {
-      const dp = new DummyPattern(null);
+      const dp = new DummyPattern('/url');
+      dp.path = 123;
       assert.equal(dp.query, '');
     });
   });
@@ -89,7 +92,8 @@ describe('DummyPattern', () => {
     });
 
     it('return empty string if path is empty', () => {
-      const dp = new DummyPattern('');
+      const dp = new DummyPattern('/url');
+      dp.path = '';
       assert.equal(dp.pathPattern, '');
     });
   });
@@ -97,7 +101,7 @@ describe('DummyPattern', () => {
   describe('parseUrlPattern', () => {
     it('set urlPattern with a UrlPattern instance', () => {
       const dp = new DummyPattern('/url');
-      parseUrlStub.restore();
+      dp.urlPattern = {};
       dp.parseUrlPattern();
       assert(dp.urlPattern instanceof UrlPattern);
     });
@@ -106,7 +110,7 @@ describe('DummyPattern', () => {
   describe('parseQueryPattern', () => {
     it('set queryPattern to key/value parsed by query string', () => {
       const dp = new DummyPattern('/url?page=1&orderBy=id');
-      parseQueryStub.restore();
+      dp.queryPattern = {};
       dp.parseQueryPattern();
       assert.deepEqual(dp.queryPattern, {
         page: 1,
@@ -116,7 +120,7 @@ describe('DummyPattern', () => {
 
     it('set queryPattern value to true if the query param value is not assigned', () => {
       const dp = new DummyPattern('/url?page=1&orderBy');
-      parseQueryStub.restore();
+      dp.queryPattern = {};
       dp.parseQueryPattern();
       assert.deepEqual(dp.queryPattern, {
         page: 1,
@@ -126,9 +130,116 @@ describe('DummyPattern', () => {
 
     it('set queryPattern an empty object if no query string', () => {
       const dp = new DummyPattern('/url');
-      parseQueryStub.restore();
+      dp.queryPattern = {};
       dp.parseQueryPattern();
       assert.deepEqual(dp.queryPattern, {});
+    });
+  });
+
+  describe('match', () => {
+    it('call mathcUrl and matchQuery with path and query parameter', () => {
+      const dp = new DummyPattern('/url?page=1&orderBy=id');
+      const matchUrlSpy = sinon.spy(dp, 'matchUrl');
+      const matchQuery = sinon.spy(dp, 'matchQuery');
+      const path = '/url';
+      const query = { page: 1 };
+      dp.match(path, query);
+      assert(matchUrlSpy.calledOnceWith(path));
+      assert(matchQuery.calledOnceWith(query));
+    });
+
+    it('return true if url and query both matches', () => {
+      const dp = new DummyPattern('/url?page=1&orderBy=id');
+      sinon.stub(dp, 'matchUrl').returns(true);
+      sinon.stub(dp, 'matchQuery').returns(true);
+      assert(dp.match('', {}));
+    });
+
+    it('return false if url not matches', () => {
+      const dp = new DummyPattern('/url?page=1&orderBy=id');
+      sinon.stub(dp, 'matchUrl').returns(false);
+      sinon.stub(dp, 'matchQuery').returns(true);
+      assert(!dp.match('', {}));
+    });
+
+    it('return false if query not matches', () => {
+      const dp = new DummyPattern('/url?page=1&orderBy=id');
+      sinon.stub(dp, 'matchUrl').returns(true);
+      sinon.stub(dp, 'matchQuery').returns(false);
+      assert(!dp.match('', {}));
+    });
+
+    it('return false if url and query both not matches', () => {
+      const dp = new DummyPattern('/url?page=1&orderBy=id');
+      sinon.stub(dp, 'matchUrl').returns(false);
+      sinon.stub(dp, 'matchQuery').returns(false);
+      assert(!dp.match('', {}));
+    });
+  });
+
+  describe('matchUrl', () => {
+    it('call urlPattern.match with path parameter', () => {
+      const dp = new DummyPattern('/url?page=1&orderBy=id');
+      const spy = sinon.spy(dp.urlPattern, 'match');
+      const path = '/url';
+      dp.matchUrl(path);
+      assert(spy.calledOnceWith(path));
+    });
+
+    it('return result of urlPattern.match', () => {
+      const dp = new DummyPattern('/url?page=1&orderBy=id');
+      const stub = sinon.stub(dp.urlPattern, 'match');
+      stub.returns(true);
+      assert(dp.matchUrl(''));
+      stub.returns(false);
+      assert(!dp.matchUrl(''));
+    });
+
+    it('return true if url equals urlPattern', () => {
+      const dp = new DummyPattern('/url');
+      assert(dp.matchUrl('/url'));
+    });
+
+    it('return true if url part matches urlPattern', () => {
+      const dp = new DummyPattern('/url?page=1&orderBy=id');
+      assert(dp.matchUrl('/url'));
+    });
+
+    it('return true if url matches urlPattern with url params', () => {
+      const dp = new DummyPattern('/url/:id?page=1&orderBy=id');
+      assert(dp.matchUrl('/url/10'));
+    });
+
+    it('return false if url matches urlPattern', () => {
+      const dp = new DummyPattern('/url?page=1&orderBy=id');
+      assert(!dp.matchUrl('/notMatchesUrl'));
+    });
+  });
+
+  describe('matchQuery', () => {
+    it('return true if query object equals queryPattern', () => {
+      const dp = new DummyPattern('/url?page=1');
+      assert(dp.matchQuery({ page: '1' }));
+    });
+
+    it('return true if query object is a superset of queryPattern', () => {
+      const dp = new DummyPattern('/url?page=1');
+      assert(dp.matchQuery({ page: '1', orderBy: 'id' }));
+    });
+
+    it('return true if query object matches queryPattern without assigned value', () => {
+      const dp = new DummyPattern('/url?page');
+      assert(dp.matchQuery({ page: '1' }));
+    });
+
+    it('return false if query object lacks url params', () => {
+      const dp = new DummyPattern('/url?page');
+      assert(!dp.matchQuery({}));
+    });
+
+    it('return false if query object with different value', () => {
+      const dp = new DummyPattern('/url?page=1');
+      assert(!dp.matchQuery({ page: '2' }));
     });
   });
 });
