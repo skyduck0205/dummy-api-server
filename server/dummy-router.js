d1b59de12a23a4fc07f35756f2baec69f5023452
skyduck <skyduck0205@gmail.com>
2020-05-22 17:08:21 +0800

    feat(server/DummyRouter): Add match methods

--- server/dummy-router.js
+++ server/dummy-router.js
@@ -6,19 +6,51 @@ class DummyRouter {
   constructor(apis) {
     this.apis = apis;
     this.patterns = [];
+
     this.updatePatterns();
   }
 
+  /**
+   * Update the list of uniq patterns
+   */
   updatePatterns() {
     this.patterns = _(this.apis)
-      .uniqBy('pathPattern')
-      .map((api) => {
-        debug('add pattern:', api.path);
-        return {
-          path: api.path,
-          pattern: new DummyPattern(api.path)
-        };
-      })
+      .uniqBy('normalizedPath')
+      .map((api) => new DummyPattern(api.path))
+      .value();
+    debug('updatePatterns:', _.map(this.patterns, 'path'));
+  }
+
+  /**
+   * Find matched API by request method, path and query
+   * @param {string} method - request method
+   * @param {string} path - request path
+   * @param {object} query - request query key-value object
+   * @returns {object} - API object
+   */
+  getMatchedAPI(method, path, query) {
+    const matchedPatterns = this.getMatchedPatterns(path, query);
+    const matchedAPIs = _(this.apis)
+      .filter({ disabled: false, method }) // filter available API with same method
+      .intersectionBy(matchedPatterns, 'path') // filter by matched patterns
+      .value();
+    debug('matchedAPIs:', _.map(matchedAPIs, 'path'));
+    return matchedAPIs[0];
+  }
+
+  /**
+   * Filter and sort the patterns by request path and query
+   * @param {string} path - path from a request
+   * @param {object} query - query parameters object from a request
+   * @returns {Pattern[]} - matched patterns
+   */
+  getMatchedPatterns(path, query) {
+    return _(this.patterns)
+      .filter((pattern) => pattern.match(path, query)) // filter matched patterns
+      .orderBy(
+        ['urlParamsLength', 'strictQueryParamsLength', 'looseQueryParamsLength'],
+        ['asc', 'desc', 'desc']
+      )
       .value();
   }
 }
---
+++ server/test/dummy-router.test.js
@@ -0,0 +1,212 @@
+const assert = require('assert');
+const sinon = require('sinon');
+const _ = require('lodash');
+const DummyRouter = require('../dummy-router');
+const DummyPattern = require('../dummy-pattern');
+
+describe('DummyRouter', () => {
+  describe('constructor', () => {
+    let updatePatternsStub;
+
+    beforeEach(() => {
+      updatePatternsStub = sinon.stub(DummyRouter.prototype, 'updatePatterns');
+    });
+
+    afterEach(() => {
+      updatePatternsStub.restore();
+    });
+
+    it('init attributes and call updatePatterns', () => {
+      const apis = [{}];
+      const dp = new DummyRouter(apis);
+      assert.equal(dp.apis, apis);
+      assert.deepEqual(dp.patterns, []);
+      assert(updatePatternsStub.called);
+    });
+  });
+
+  describe('updatePattern', () => {
+    let updatePatternsStub;
+
+    beforeEach(() => {
+      updatePatternsStub = sinon.stub(DummyRouter.prototype, 'updatePatterns');
+    });
+
+    afterEach(() => {
+      updatePatternsStub.restore();
+    });
+
+    it('pattern items are instances of DummyPattern', () => {
+      const apis = [
+        { path: '/api/0', normalizedPath: '/api/0' },
+        { path: '/api/1', normalizedPath: '/api/1' }
+      ];
+      const dp = new DummyRouter(apis);
+      updatePatternsStub.restore();
+      dp.updatePatterns();
+      assert(_.every(dp.patterns, p => p instanceof DummyPattern));
+    });
+
+    it('should remove duplicated paths', () => {
+      const apis = [
+        { path: '/api/0', normalizedPath: '/api/0' },
+        { path: '/api/0', normalizedPath: '/api/0' },
+        { path: '/api/1', normalizedPath: '/api/1' }
+      ];
+      const dp = new DummyRouter(apis);
+      updatePatternsStub.restore();
+      dp.updatePatterns();
+      assert(dp.patterns.length === 2);
+      assert.equal(dp.patterns[0].path, '/api/0');
+      assert.equal(dp.patterns[1].path, '/api/1');
+    });
+  });
+
+  describe('getMatchedPatterns', () => {
+    let matchStub;
+
+    beforeEach(() => {
+      matchStub = sinon.stub(DummyPattern.prototype, 'match');
+    });
+
+    afterEach(() => {
+      matchStub.restore();
+    });
+
+    describe('filter matched patterns', () => {
+      it('get all matched patterns', () => {
+        const apis = [
+          { path: '/api/0', normalizedPath: '/api/0' },
+          { path: '/api/1', normalizedPath: '/api/1' },
+          { path: '/api/2', normalizedPath: '/api/2' }
+        ];
+        const dp = new DummyRouter(apis);
+        matchStub.returns(true);
+        const matched = dp.getMatchedPatterns('', {});
+        assert(matched.length === 3);
+        assert.equal(matched[0].path, '/api/0');
+        assert.equal(matched[1].path, '/api/1');
+        assert.equal(matched[2].path, '/api/2');
+      });
+
+      it('drop patterns which DummyPattern.match return false', () => {
+        const apis = [
+          { path: '/api/0', normalizedPath: '/api/0' },
+          { path: '/api/1', normalizedPath: '/api/1' },
+          { path: '/api/2', normalizedPath: '/api/2' }
+        ];
+        const dp = new DummyRouter(apis);
+        matchStub
+          .onFirstCall().returns(false)
+          .onSecondCall().returns(false)
+          .onThirdCall().returns(true);
+        const matched = dp.getMatchedPatterns('', {});
+        assert(matched.length === 1);
+        assert.equal(matched[0].path, '/api/2');
+      });
+    });
+
+    describe('order patterns', () => {
+      beforeEach(() => {
+        matchStub.returns(true);
+      });
+
+      it('first order by urlParamsLength(asc)', () => {
+        const mockPatterns = [
+          { urlParamsLength: 2, strictQueryParamsLength: 0, looseQueryParamsLength: 10, match: () => true },
+          { urlParamsLength: 3, strictQueryParamsLength: 10, looseQueryParamsLength: 0, match: () => true },
+          { urlParamsLength: 1, strictQueryParamsLength: 0, looseQueryParamsLength: 20, match: () => true },
+        ];
+        const dp = new DummyRouter([]);
+        dp.patterns = mockPatterns;
+        const matched = dp.getMatchedPatterns('', {});
+        assert.deepEqual(_.map(matched, 'urlParamsLength'), [1, 2, 3]);
+      });
+
+      it('second order by strictQueryParamsLength(desc)', () => {
+        const mockPatterns = [
+          { urlParamsLength: 0, strictQueryParamsLength: 2, looseQueryParamsLength: 20, match: () => true },
+          { urlParamsLength: 0, strictQueryParamsLength: 3, looseQueryParamsLength: 0, match: () => true },
+          { urlParamsLength: 0, strictQueryParamsLength: 1, looseQueryParamsLength: 10, match: () => true },
+        ];
+        const dp = new DummyRouter([]);
+        dp.patterns = mockPatterns;
+        const matched = dp.getMatchedPatterns('', {});
+        assert.deepEqual(_.map(matched, 'strictQueryParamsLength'), [3, 2, 1]);
+      });
+
+      it('third order by looseQueryParamsLength(desc)', () => {
+        const mockPatterns = [
+          { urlParamsLength: 0, strictQueryParamsLength: 0, looseQueryParamsLength: 2, match: () => true },
+          { urlParamsLength: 0, strictQueryParamsLength: 0, looseQueryParamsLength: 3, match: () => true },
+          { urlParamsLength: 0, strictQueryParamsLength: 0, looseQueryParamsLength: 1, match: () => true },
+        ];
+        const dp = new DummyRouter([]);
+        dp.patterns = mockPatterns;
+        const matched = dp.getMatchedPatterns('', {});
+        assert.deepEqual(_.map(matched, 'looseQueryParamsLength'), [3, 2, 1]);
+      });
+    });
+  });
+
+  describe('getMatchedAPI', () => {
+    let matchPatternStub;
+
+    beforeEach(() => {
+      matchPatternStub = sinon.stub(DummyRouter.prototype, 'getMatchedPatterns')
+        .returns([
+          { path: '/api/0' },
+          { path: '/api/1' },
+          { path: '/api/2' }
+        ]);
+    });
+
+    afterEach(() => {
+      matchPatternStub.restore();
+    });
+
+    it('filter disabled API', () => {
+      const apis = [
+        { method: 'GET', path: '/api/0', disabled: true },
+        { method: 'GET', path: '/api/1', disabled: true },
+        { method: 'GET', path: '/api/2', disabled: false }
+      ];
+      const dp = new DummyRouter(apis);
+      const api = dp.getMatchedAPI('GET', '', {});
+      assert.equal(api, apis[2]);
+    });
+
+    it('filter APIs with method', () => {
+      const apis = [
+        { method: 'GET', path: '/api/0', disabled: true },
+        { method: 'POST', path: '/api/1', disabled: false },
+        { method: 'GET', path: '/api/2', disabled: false }
+      ];
+      const dp = new DummyRouter(apis);
+      const api = dp.getMatchedAPI('GET', '', {});
+      assert.equal(api, apis[2]);
+    });
+
+    it('filter APIs by intersection of patterns paths and apis paths', () => {
+      const apis = [
+        { method: 'GET', path: '/api/20', disabled: false },
+        { method: 'GET', path: '/api/1', disabled: false },
+        { method: 'GET', path: '/api/30', disabled: false }
+      ];
+      const dp = new DummyRouter(apis);
+      const api = dp.getMatchedAPI('GET', '', {});
+      assert.equal(api, apis[1]);
+    });
+
+    it('return first matched API', () => {
+      const apis = [
+        { method: 'GET', path: '/api/0', disabled: false },
+        { method: 'GET', path: '/api/1', disabled: false },
+        { method: 'GET', path: '/api/2', disabled: false }
+      ];
+      const dp = new DummyRouter(apis);
+      const api = dp.getMatchedAPI('GET', '', {});
+      assert.equal(api, apis[0]);
+    });
+  });
+});
