f3d512e943db14f3af777987a8ab3973f7fdc722
skyduck <skyduck0205@gmail.com>
2020-07-13 17:52:20 +0800

    feat(web/ApiEditModal, ApiList): update ApiEditModal and ApiList

--- web/src/App/ApiEditModal/ApiEditModal.js
+++ web/src/App/ApiEditModal/ApiEditModal.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { get as _get } from 'lodash';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
 import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
 import Fade from '@material-ui/core/Fade';
 import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
 import ModalHeader from 'components/ModalHeader';
 import ModalFooter from 'components/ModalFooter';
+import genId from '../../../../utils/genId';
+import ApiMethodSelector from './ApiMethodSelector';
+import ApiResponseEditor from './ApiResponseEditor';
 
 const useStyles = makeStyles((theme) => ({
   modal: {
@@ -17,16 +27,131 @@ const useStyles = makeStyles((theme) => ({
     margin: theme.spacing(5, 'auto'),
   },
   container: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
     outline: 'none',
+  },
+  paper: {
+    display: 'flex',
+    flexDirection: 'column',
+    maxHeight: '100%',
   },
 }));
 
-function ApiEditModal({ open, api, onCancelClick }) {
+const defaultForm = {
+  id: null,
+  method: 'GET',
+  path: '',
+  description: '',
+  responses: [],
+  currentResponseID: null,
+};
+
+function ApiEditModal({ open, api, onOk, onCancel }) {
   const classes = useStyles();
+  const [form, setForm] = React.useState(defaultForm);
+  const [expended, setExpanded] = React.useState(null);
+
+  React.useEffect(() => {
+    if (open) {
+      if (api) {
+        setForm({
+          id: api.id,
+          method: api.method,
+          path: api.path,
+          description: api.description,
+          responses: api.responses,
+          currentResponseID: api.currentResponseID,
+        });
+      } else {
+        setForm(defaultForm);
+      }
+    }
+  }, [open, api]);
+
+  const onFormChange = (key, value) => {
+    setForm({
+      ...form,
+      [key]: value,
+    });
+  };
+
+  const onExpandedChange = (id, isExpanded) => {
+    setExpanded(isExpanded ? id : null);
+  };
+
+  const onAddResponseClick = () => {
+    const newResponse = { id: genId(), name: '', status: 200, body: {} };
+    const responses = [...form.responses, newResponse];
+    let currentResponseID = form.currentResponseID;
+    if (!currentResponseID) {
+      currentResponseID = newResponse.id;
+    }
+    setExpanded(newResponse.id);
+    setForm({
+      ...form,
+      responses,
+      currentResponseID,
+    });
+  };
+
+  const onResponseChange = (index, value) => {
+    const responses = [
+      ...form.responses.slice(0, index),
+      value,
+      ...form.responses.slice(index + 1),
+    ];
+    setForm({ ...form, responses });
+  };
+
+  const onResponseCopyClick = (index) => {
+    const response = form.responses[index];
+    if (!response) {
+      return;
+    }
+    const newResponse = {
+      ...response,
+      name: `${response.name} (copied)`,
+      id: genId(),
+    };
+    const responses = [...form.responses, newResponse];
+    setExpanded(newResponse.id);
+    setForm({ ...form, responses });
+  };
+
+  const onResponseDeleteClick = (index) => {
+    const response = form.responses[index];
+    if (!response) {
+      return;
+    }
+    const responses = [
+      ...form.responses.slice(0, index),
+      ...form.responses.slice(index + 1),
+    ];
+    const isCurrent = response.id === form.currentResponseID;
+    let currentResponseID = form.currentResponseID;
+    if (isCurrent) {
+      currentResponseID = _get(responses, '[0].id', null);
+    }
+    setForm({
+      ...form,
+      responses,
+      currentResponseID,
+    });
+  };
+
+  const onOkClick = () => {
+    // TODO: Request for add/update API
+    onOk(form);
+  };
+
   return (
     <Modal
       className={classes.modal}
       open={open}
+      BackdropComponent={Backdrop}
+      BackdropProps={{ timeout: 200 }}
       closeAfterTransition
       disableAutoFocus
       disableEnforceFocus
@@ -34,20 +159,75 @@ function ApiEditModal({ open, api, onCancelClick }) {
     >
       <Fade in={open}>
         <Container className={classes.container} maxWidth="md">
-          <Paper square elevation={5}>
+          <Paper className={classes.paper} square elevation={5}>
             {/* modal header */}
             <ModalHeader
               title={api ? 'Edit API' : 'Add API'}
-              onCloseClick={onCancelClick}
+              onCloseClick={onCancel}
             />
 
             {/* modal body */}
-            <Box p={3} py={2}>
-              modal content
+            <Box p={3} py={2} overflow="auto">
+              {/* method */}
+              <ApiMethodSelector
+                value={form.method}
+                onChange={(e) => onFormChange('method', e.target.value)}
+              />
+              {/* path */}
+              <TextField
+                fullWidth
+                required
+                id="das-api-path"
+                label="Path"
+                size="small"
+                margin="normal"
+                value={form.path}
+                onChange={(e) => onFormChange('path', e.target.value)}
+                placeholder="API path e.g. /api/user/:userId"
+                helperText="API path with method should be unique and not empty."
+                autoFocus
+              />
+              {/* description */}
+              <TextField
+                fullWidth
+                id="das-api-description"
+                label="Description"
+                size="small"
+                margin="normal"
+                value={form.description}
+                onChange={(e) => onFormChange('description', e.target.value)}
+                placeholder="API description"
+              />
+              <FormControl margin="normal" fullWidth required>
+                <FormLabel component="legend">Responses</FormLabel>
+                <Box my={1}>
+                  {form.responses.map((response, index) => (
+                    <ApiResponseEditor
+                      key={response.id}
+                      response={response}
+                      isCurrent={response.id === form.currentResponseID}
+                      isExpanded={expended === response.id}
+                      onExpandedChange={(e, isExpanded) => {
+                        onExpandedChange(response.id, isExpanded);
+                      }}
+                      onChange={(value) => onResponseChange(index, value)}
+                      onCopyClick={() => onResponseCopyClick(index)}
+                      onDeleteClick={() => onResponseDeleteClick(index)}
+                    />
+                  ))}
+                </Box>
+                <Button
+                  color="secondary"
+                  startIcon={<Icon>add</Icon>}
+                  onClick={onAddResponseClick}
+                >
+                  Add Response
+                </Button>
+              </FormControl>
             </Box>
 
             {/* modal footer */}
-            <ModalFooter onCancelClick={onCancelClick} />
+            <ModalFooter onOkClick={onOkClick} onCancelClick={onCancel} />
           </Paper>
         </Container>
       </Fade>
@@ -58,13 +238,15 @@ function ApiEditModal({ open, api, onCancelClick }) {
 ApiEditModal.propTypes = {
   open: PropTypes.bool,
   api: PropTypes.object,
-  onCancelClick: PropTypes.func,
+  onOk: PropTypes.func,
+  onCancel: PropTypes.func,
 };
 
 ApiEditModal.defaultProps = {
   open: false,
   api: null,
-  onCancelClick: () => {},
+  onOk: () => {},
+  onCancel: () => {},
 };
 
 export default ApiEditModal;
--- web/src/App/ApiList/ApiList.js
+++ web/src/App/ApiList/ApiList.js
@@ -1,18 +1,38 @@
 import React from 'react';
+import { useSnackbar } from 'notistack';
 import MaterialTable from 'material-table';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
-import { useSnackbar } from 'notistack';
+import IconButton from '@material-ui/core/IconButton';
+import Icon from '@material-ui/core/Icon';
+import Tooltip from '@material-ui/core/Tooltip';
+import Fab from '@material-ui/core/Fab';
 import useApi from 'hooks/useApi';
 import api from 'services/api';
 import HttpStatus from 'components/HttpStatus';
 import ApiEditModal from 'App/ApiEditModal';
+
+const useStyles = makeStyles((theme) => ({
+  actions: {
+    '& button + button': {
+      marginLeft: theme.spacing(1),
+    },
+  },
+  fab: {
+    position: 'fixed',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+}));
 
 function ApiList() {
   const [apis, setAPIs] = React.useState([]);
   const [selectedAPI, setSelectedAPI] = React.useState(null);
   const [isEditModalOpen, setIsEditModalOpen] = React.useState(false);
 
+  const classes = useStyles();
   const { enqueueSnackbar } = useSnackbar();
 
   const [listAPIsStatus, listAPIsFetch] = useApi(api.listAPIs, {
@@ -39,18 +59,13 @@ function ApiList() {
         variant: 'error',
       });
     }
-  }, [
-    updateAPIResponseStatus.data,
-    updateAPIResponseStatus.error,
-    listAPIsFetch,
-    enqueueSnackbar,
-  ]);
+  }, [updateAPIResponseStatus, listAPIsFetch, enqueueSnackbar]);
 
   React.useEffect(() => {
     if (listAPIsStatus.data) {
       setAPIs(listAPIsStatus.data.data);
     }
-  }, [listAPIsStatus.data]);
+  }, [listAPIsStatus]);
 
   const onResponseSelect = React.useCallback(
     (apiID, responseID) => {
@@ -58,6 +73,19 @@ function ApiList() {
     },
     [updateAPIResponseFetch]
   );
+
+  const onEditClick = (rowData) => {
+    setSelectedAPI(rowData);
+    setIsEditModalOpen(true);
+  };
+
+  const onCopyClick = (rowData) => {
+    console.log(rowData);
+  };
+
+  const onDeleteClick = (rowData) => {
+    console.log(rowData);
+  };
 
   return (
     <>
@@ -75,7 +103,10 @@ function ApiList() {
           {
             title: 'API Path',
             field: 'path',
-            cellStyle: { fontSize: 'initial' },
+            cellStyle: {
+              fontSize: 'initial',
+              fontFamily: 'monospace',
+            },
           },
           {
             title: 'Response',
@@ -102,49 +133,56 @@ function ApiList() {
             field: 'description',
             cellStyle: { fontSize: 'initial' },
           },
+          {
+            title: 'Actions',
+            sorting: false,
+            render: (rowData) => (
+              <Box className={classes.actions}>
+                <Tooltip title="Edit API">
+                  <IconButton size="small" onClick={() => onEditClick(rowData)}>
+                    <Icon fontSize="small">edit</Icon>
+                  </IconButton>
+                </Tooltip>
+                <Tooltip title="Copy API">
+                  <IconButton size="small" onClick={() => onCopyClick(rowData)}>
+                    <Icon fontSize="small">file_copy</Icon>
+                  </IconButton>
+                </Tooltip>
+                <Tooltip title="Delete API">
+                  <IconButton
+                    size="small"
+                    onClick={() => onDeleteClick(rowData)}
+                  >
+                    <Icon fontSize="small">delete</Icon>
+                  </IconButton>
+                </Tooltip>
+              </Box>
+            ),
+          },
         ]}
         data={apis}
         options={{
           paging: false,
-          actionsColumnIndex: -1,
           headerStyle: { fontSize: 'initial' },
         }}
-        actions={[
-          {
-            icon: 'add',
-            tooltip: 'Add API',
-            isFreeAction: true,
-            onClick: () => {
-              setSelectedAPI(null);
-              setIsEditModalOpen(true);
-            },
-          },
-          {
-            icon: 'edit',
-            tooltip: 'Edit API',
-            onClick: (e, rowData) => {
-              setSelectedAPI(rowData);
-              setIsEditModalOpen(true);
-            },
-          },
-          {
-            icon: 'file_copy',
-            tooltip: 'Copy API',
-            onClick: (e, rowData) => console.log(`Copy ${rowData.id}`),
-          },
-          {
-            icon: 'delete',
-            tooltip: 'Delete API',
-            onClick: (e, rowData) => console.log(`Delete ${rowData.id}`),
-          },
-        ]}
       />
 
       <ApiEditModal
         open={isEditModalOpen}
         api={selectedAPI}
-        onCancelClick={() => setIsEditModalOpen(false)}
+        onOk={(value) => console.log('onOk', value)}
+        onCancel={() => setIsEditModalOpen(false)}
       />
+
+      <Tooltip title="Add API">
+        <Fab
+          className={classes.fab}
+          color="primary"
+          onClick={() => onEditClick()}
+        >
+          <Icon>add</Icon>
+        </Fab>
+      </Tooltip>
     </>
   );
 }
