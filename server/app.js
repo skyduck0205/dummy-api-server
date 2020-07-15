fe3a3996a79f6989b168dae5f6941692f74df4ac
skyduck <skyduck0205@gmail.com>
2020-07-15 16:39:20 +0800

    feat(server/app): add get DB api

--- server/app.js
+++ server/app.js
@@ -39,6 +39,9 @@ app.use(express.static(PUBLIC_PATH));
 /**
  * Dummy Server APIs
  */
+app.route(`${DS_PREFIX}/db`)
+  .get((req, res) => res.send(db.value()));
+
 app.route(`${DS_PREFIX}/apis`)
   // get api list
   .get((req, res) => res.send({
